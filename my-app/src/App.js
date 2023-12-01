8c89ab3aa125c6a08a522397b8bfe067af2388a6
Minami <77323035+Minamichan716@users.noreply.github.com>
2023-12-01 14:37:01 +0900

    12/1

---
+++ my-app/src/App.js
@@ -0,0 +1,43 @@
+
+import './App.css';
+import React, { useState } from 'react'
+
+const title = '寿命カウンター'
+
+
+function App() {
+// 処理の内容を書く
+// const [count,setCount] = useState(0)
+const [age, setAge] = useState(0);
+const resetAge = () => {
+  setAge('')
+}
+const calcAge = () => {
+  setAge((age) => 88 - age )
+}
+
+
+
+  return (
+    <>
+        <div className="App">
+      <h1>{title}</h1>
+        <p>あなたの年齢を入力してください</p>
+
+    <input value={age}
+        onChange={(event) => setAge(event.target.value)} />&nbsp;歳<br></br>
+
+
+      <h2>寿命は残り{age}年です</h2>
+      <button onClick={resetAge}>リセット</button>
+      <button onClick={calcAge}>計算する</button>
+    </div>
+    </>
+
+  );
+}
+
+
+
+
+export default App;
